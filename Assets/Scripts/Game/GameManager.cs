d74cfa8eb79f3e8f7d6bff0040eff034fcfe533b
Kritula <deryavanly@gmail.com>
2026-01-31 22:00:19 +0500

    DZ_3_Urok_Sdelala
    Dobavlen_CharacterSpawnnController

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -11,15 +11,16 @@ namespace OmniumLessons
         
         private bool _isGameActive = false;
         private  float _gameTimeSec = 0;
-        private  float _timeBetweenEnemySpawn = 0;
         
         public static GameManager Instance { get; private set; }
-        
         
         public ScoreManager ScoreManager { get; private set; }
         
         public CharacterFactory CharacterFactory => _characterFactory;
-        
+        public GameData GameData => _gameData;
+
+        private CharacterSpawnController _spawnController;
+
         private void Awake()
         {
             if (Instance == null)
@@ -50,12 +51,15 @@ namespace OmniumLessons
             var player = CharacterFactory.CreateCharacter(CharacterType.DefaultPlayer);
             player.transform.position = Vector3.zero;
             player.gameObject.SetActive(true);
-            player.LiveComponent.OnCharacterDeath += OnCharacterDeathHandler;
+            RegisterCharacter(player);
 
             _gameTimeSec = 0f;
-            _timeBetweenEnemySpawn = _gameData.TimeBetweenEnemySpawn;
             ScoreManager.StartGame();
-        
+
+            // 4) Запускаем спавн-контроллер
+            _spawnController = new CharacterSpawnController();
+            _spawnController.StartSpawn();
+
             _isGameActive = true;
         }
 
@@ -65,18 +69,23 @@ namespace OmniumLessons
                 return;
             
             _gameTimeSec += Time.deltaTime;
-            _timeBetweenEnemySpawn += Time.deltaTime;
-        
-            if (_timeBetweenEnemySpawn >= _gameData.TimeBetweenEnemySpawn)
-            {
-                SpawnEnemy();
-                _timeBetweenEnemySpawn = 0;
-            }
-        
+
+            // Спавн теперь живёт здесь
+            _spawnController.OnUpdate(Time.deltaTime);
+
             if (_gameTimeSec >= _gameData.GameTimeSecondsMax)
             {
                 GameVictory();
             }
+        }
+
+        // ВАЖНО: сюда будет обращаться SpawnController после создания врага
+        public void RegisterCharacter(Character character)
+        {
+            if (character == null || character.LiveComponent == null)
+                return;
+
+            character.LiveComponent.OnCharacterDeath += OnCharacterDeathHandler;
         }
 
         private void OnCharacterDeathHandler(Character deathCharacter)
@@ -95,35 +104,11 @@ namespace OmniumLessons
         
             CharacterFactory.ReturnCharacterToPool(deathCharacter);
             deathCharacter.gameObject.SetActive(false);
-        
+
+            // отписка
             deathCharacter.LiveComponent.OnCharacterDeath -= OnCharacterDeathHandler;
         }
         
-        private void SpawnEnemy()
-        {
-            var character = CharacterFactory.CreateCharacter(CharacterType.DefaultEnemy);
-
-            float posX = CharacterFactory.Player.transform.position.x + GetOffset();
-            float posZ = CharacterFactory.Player.transform.position.z + GetOffset();
-            Vector3 spawnPoint = new Vector3(posX, 0, posZ);
-            character.transform.position = spawnPoint;
-            
-            character.LiveComponent.OnCharacterDeath += OnCharacterDeathHandler;
-            character.gameObject.SetActive(true);
-
-        
-            
-            float GetOffset()
-            {
-                bool isPlus = Random.Range(0, 1) > 0;
-                float randomOffset = Random.Range(_gameData.MinEnemySpawnOffset, _gameData.MaxEnemySpawnOffset);
-                
-                return isPlus
-                    ? randomOffset
-                    : -randomOffset;
-            }
-        }
-
         private void GameOver()
         {
             Debug.Log("GameOver!");
@@ -131,6 +116,9 @@ namespace OmniumLessons
             Debug.Log("ScoreMax = " + ScoreManager.ScoreMax);
             ScoreManager.CompleteMatch();
             _isGameActive = false;
+
+            // останавливаем спавн
+            _spawnController.StopSpawn();
         }
 
         private void GameVictory()
@@ -138,6 +126,9 @@ namespace OmniumLessons
             Debug.Log("Game Over! Time's up!");
             ScoreManager.CompleteMatch();
             _isGameActive = false;
+
+            // останавливаем спавн
+            _spawnController.StopSpawn();
         }
     }
 }
